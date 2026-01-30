ab7cd99bbf4a3382cf934a8a268799ed6aee8cdf
Anji Babu (LTIMINDTREE LIMITED) <v-anjibabu@microsoft.com>
2026-01-30 12:00:51 +0530

    changed the valuse for multi vm deployment

--- templetes/backend.tf
+++ templetes/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    // resource_group_name  = "test-rg"
-    // storage_account_name = "testaccountsampltf"
-    // container_name       = "sampletfcontainer"
-    // key                  = "terraform.tfstate"
+    resource_group_name  = "test-rg"
+    storage_account_name = "testaccountsampltf"
+    container_name       = "sampletfcontainer"
+    key                  = "terraform.tfstate"
   }
 }
